4f1ca8cf73572062d96c40a00adab15d5a31106c
ouranossystems <63326044+ouranossystems@users.noreply.github.com>
2021-08-03 23:01:06 +1000

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
-  $('.customer-logos').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows: false,
-    dots: false,
-    pauseOnHover: false,
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2
-      }
-    }, {
-      breakpoint: 520,
-      settings: {
-        slidesToShow: 1
-      }
-    }]
-  });
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
 });
